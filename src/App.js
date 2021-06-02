a874be74a9a0173526c683eb13e75337bf675501
Justin Park <justin.park@airbnb.com>
2021-06-02 11:03:18 -0700

    컨텍스트 데모 코드

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import HomePageComponent from './context/HomePageComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <HomePageComponent />
+    </div>
+  );
+}
+
+export default App;
