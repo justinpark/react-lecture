97239246e3bedfd910dd93d54fecda33e95ceaad
Just[in]Do it! <justin.park@airbnb.com>
2022-01-07 10:41:50 -0800

    프로퍼티 변수 변경

--- src/context/Button.jsx
+++ src/context/Button.jsx
@@ -1,16 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export default function Button({
-    children,
-    isLoading,
-    onPress,
-  }) {
+export default function Button(props) {
+  const { isLoading, children, onPress } = props;
+
   return (
-    <button
-      disabled={isLoading}
-      onClick={onPress}
-    >
+    <button disabled={isLoading} onClick={onPress}>
       {isLoading ? '처리중...' : children}
     </button>
   );
@@ -19,9 +14,9 @@ export default function Button({
 Button.propTypes = {
   children: PropTypes.node.isRequired,
   isLoading: PropTypes.bool,
-  onPress: PropTypes.func,
+  onPress: PropTypes.func
 };
 
 Button.defaultProps = {
-  onPress: () => {},
+  onPress: () => {}
 };
